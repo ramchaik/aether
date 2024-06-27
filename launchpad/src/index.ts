b5f92e1122e935ed6f036bb3ef8f1ae4c188fa39
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2024-06-26 22:25:50 -0300

    feat: add dockerfile

--- launchpad/src/index.ts
+++ launchpad/src/index.ts
@@ -2,23 +2,27 @@ import Fastify from "fastify";
 import registerRoutes from "./routes";
 import prisma from "./lib/prisma";
 
-const PORT = (process.env.PORT || 3000) as number;
+const PORT = (process.env.PORT || 8000) as number;
 
-const fastify = Fastify({
+const app = Fastify({
   logger: true,
 });
 
-fastify.register(registerRoutes);
+app.register(registerRoutes);
 
-fastify.addHook("onClose", async () => {
+app.get("/health", async (req, reply) => {
+  reply.code(200).send({ message: "Server is healthy!" });
+});
+
+app.addHook("onClose", async () => {
   await prisma.$disconnect();
 });
 
 async function start() {
   try {
-    await fastify.listen({ port: PORT });
+    await app.listen({ host: "0.0.0.0", port: PORT });
   } catch (error) {
-    fastify.log.error(error);
+    app.log.error(error);
     process.exit(1);
   }
 }
