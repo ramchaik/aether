fbcf6c4a3b8083b705222768e08abe407c196f66
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2024-07-18 17:52:32 -0300

    feat(tf): add policy to s3 bucket for public read

--- tf/main.tf
+++ tf/main.tf
@@ -122,6 +122,32 @@ resource "aws_s3_bucket" "aether" {
   }
 }
 
+resource "aws_s3_bucket_policy" "aether_bucket_policy" {
+  bucket = aws_s3_bucket.aether.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.aether.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_public_access_block" "aether" {
+  bucket = aws_s3_bucket.aether.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_sqs_queue" "aether" {
   name = "aether-queue"
 }
