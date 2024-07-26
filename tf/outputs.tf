acc090186c415113f982f54701089f26a4f3d7fd
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2024-07-26 14:47:56 -0300

    fix(tf): minor

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -18,26 +18,6 @@ output "sqs_queue_url" {
   value = aws_sqs_queue.aether.url
 }
 
-output "forge_ecr_repository_url" {
-  value = aws_ecr_repository.forge.repository_url
-}
-
-output "launchpad_ecr_repository_url" {
-  value = aws_ecr_repository.launchpad.repository_url
-}
-
-output "frontstage_ecr_repository_url" {
-  value = aws_ecr_repository.frontstage.repository_url
-}
-
-output "logify_ecr_repository_url" {
-  value = aws_ecr_repository.logify.repository_url
-}
-
-output "proxy_ecr_repository_url" {
-  value = aws_ecr_repository.proxy.repository_url
-}
-
 output "kinesis_stream_arn" {
   value = aws_kinesis_stream.aether.arn
 }
